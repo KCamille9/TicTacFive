3c45b30c21ea8c9d0a6a19c64e7fc2ced6845b1a
Camille Alavarado Plasenscia <bgi_camille@hotmail.com>
2017-12-02 23:22:35 -0700

    First stuff

---
+++ TicTacToe/View/MainPage.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.ServiceModel.Channels;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        bool turn = true; //true = x, false = o
+        int turn_count = 0;
+
+        MessageDialog msgDialog;
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button_click(Object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Content = "X";
+            }
+            else
+            {
+                b.Content = "O";
+            }
+
+            turn = !turn;
+            b.IsEnabled = false; //Para que no puedas cambiar el simbolo en el mismo boton
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void checkForWinner()
+        {
+            bool there_is_winner = false;
+
+
+            //Horizontal Checks
+            if ((A1.Content == A2.Content) && (A2.Content == A3.Content) && (A3.Content == A4.Content) && (A4.Content == A5.Content) &&(!A1.IsEnabled))
+            {
+
+                there_is_winner = true;
+            }
+            else if ((B1.Content == B2.Content) && (B2.Content == B3.Content) && (B3.Content == B4.Content) && (B4.Content == B5.Content) &&(!B1.IsEnabled)) {
+
+                there_is_winner = true;
+            }
+            else if ((C1.Content == C2.Content) && (C2.Content == C3.Content) && (C3.Content == C4.Content) && (C4.Content == C5.Content) && (!C1.IsEnabled)) {
+
+                there_is_winner = true;
+            }
+            else if ((D1.Content == D2.Content) && (D2.Content == D3.Content) && (D3.Content == D4.Content) && (D4.Content == D5.Content) && (!D1.IsEnabled)) {
+
+                there_is_winner = true;
+            }
+            else if ((E1.Content == E2.Content) && (E2.Content == E3.Content) && (E3.Content == E4.Content) && (E4.Content == E5.Content) && (!E1.IsEnabled)) { 
+            
+                there_is_winner = true;
+            }
+
+
+            //Vertical Checks
+            if ((A1.Content == B1.Content) && (B1.Content == C1.Content) && (C1.Content == D1.Content) && (D1.Content == E1.Content) && (!A1.IsEnabled))
+            {
+
+                there_is_winner = true;
+            }
+            else if ((A2.Content == B2.Content) && (B2.Content == C2.Content) && (C2.Content == D2.Content) && (D2.Content == E2.Content) && (!A2.IsEnabled))
+            {
+
+                there_is_winner = true;
+            }
+            else if ((A3.Content == B3.Content) && (B3.Content == C3.Content) && (C3.Content == D3.Content) && (D3.Content == E3.Content) && (!A3.IsEnabled))
+            {
+
+                there_is_winner = true;
+            }
+            else if ((A4.Content == B4.Content) && (B4.Content == C4.Content) && (C4.Content == D4.Content) && (D4.Content == E4.Content) && (!A4.IsEnabled))
+            {
+
+                there_is_winner = true;
+            }
+            else if ((A5.Content == B5.Content) && (B5.Content == C5.Content) && (C5.Content == D5.Content) && (D5.Content == E5.Content) && (!A5.IsEnabled))
+            {
+
+                there_is_winner = true;
+            }
+
+            //Diagonal Checks
+            if ((A1.Content == B2.Content) && (B2.Content == C3.Content) && (C3.Content == D4.Content) && (D4.Content == E5.Content) && (!A1.IsEnabled))
+            {
+
+                there_is_winner = true;
+            }
+            else if ((E1.Content == D2.Content) && (D2.Content == C3.Content) && (C3.Content == B4.Content) && (B4.Content == A5.Content) && (!E1.IsEnabled))
+            {
+
+                there_is_winner = true;
+            }
+
+
+            if (there_is_winner)
+            {
+                disableButtons();
+                String winner = "";
+                if (turn)
+                {
+                    winner = "O";
+                }
+                else
+                {
+                    winner = "X";
+                }
+
+                //Display if X or O won
+                msgDialog = new MessageDialog(winner + " wins!!", "Congrats!");
+                msgDialog.ShowAsync();
+
+                //Show continue or exit options
+                msgDialog.Commands.Add(new UICommand("Play again", new UICommandInvokedHandler(this.CommandInvokedHandler1)));
+                msgDialog.Commands.Add(new UICommand("Close", new UICommandInvokedHandler(this.CommandInvokedHandler2)));
+            }
+            else
+            {
+                if(turn_count == 25)
+                {
+                    
+                    msgDialog = new MessageDialog("It's a draw!!", "So sad :(");
+                    msgDialog.ShowAsync();
+
+                    msgDialog.Commands.Add(new UICommand("Play again", new UICommandInvokedHandler(this.CommandInvokedHandler1)));
+                    msgDialog.Commands.Add(new UICommand("Close", new UICommandInvokedHandler(this.CommandInvokedHandler2)));
+
+                }
+            }
+
+
+        }//end check for winner
+
+        //Restore Screen
+        private void CommandInvokedHandler1(IUICommand command)
+        {
+            foreach (Button button in GameGrid.Children)
+            {
+                Button b = (Button)button;
+                b.IsEnabled = true;
+                b.Content = "";
+                turn_count = 0;
+            }
+        }
+
+        private void CommandInvokedHandler2(IUICommand command)
+        {
+            CoreApplication.Exit();
+        }
+
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Button button in GameGrid.Children)
+                {
+                    Button b = (Button)button;
+                    b.IsEnabled = false;
+                }
+            }
+            catch { }
+
+        }
+    }
+}
---
+++ TicTacToeTests/UnitTest.cs
@@ -0,0 +1,130 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TicTacToe
+{
+    [TestClass]
+    public class TicTacToeTests
+    {
+        [TestMethod]
+        public void A_User_Needs_To_Play_With_Zero_Players()
+        {
+            String promptUser = "With how many user do you want play?"; //Use checkmarks on this one
+
+            zeroPlayerskChecked = true;
+            String zeroPlayers = "You selected Zero players, the computer will compete agains another computer";
+            String userSelection = zeroPlayers;
+            Assert.AreEqual("You selected Zero players, the computer will compete agains another computer", userSelection);
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_Play_With_One_Players()
+        {
+
+            String promptUser = "With how many user do you want play?"; //Use checkmarks on this one
+
+            onePlayerChecked = true;
+            String zeroPlayers = "You selected One player, you will comptete against the computer";
+            String userSelection = onePlayers;
+            Assert.AreEqual("You selected One player, you will comptete against the computer", userSelection);
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_Play_With_Two_Players()
+        {
+            String promptUser = "With how many user do you want play?"; //Use checkmarks on this one
+
+            twoPayersChecked = true;
+            String twoPlayers = "You selected Two players, you will comptete against other plauer";
+            String userSelection = twoPlayers;
+            Assert.AreEqual("You selected Two players, you will comptete against other player", userSelection);
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_Choose_X()
+        {
+
+            User.Symbol = symbol;
+            User.Symbol = "X";
+            Assert.AreEqual("X", User.Symbol);
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_Choose_O()
+        { 
+            User.Symbol = symbol;
+            User.Symbol = "O";
+            Assert.AreEqual("O", User.Symbol);
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_Place_X()
+        {
+            User.Symbol = "X";
+            String userPosition = Button b.Content;
+            String userPosition = A1;
+            Assert.Equals("X", userPosition);
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_Place_O()
+        {
+            User.Symbol = "O";
+            String userPosition = Button b.Content;
+            String userPosition = A1;
+            Assert.Equals("O", userPosition);
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_Be_Told_When_A_Win_Happens()
+        {
+            there_is_winner = true;
+
+            Message msgDialog = new MessageDialog(winner + " wins!!", "Congrats!");
+            msgDialog.ShowAsync();
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_Be_Told_When_A_Loss_Happens()
+        {
+
+            there_is_winner = true;
+
+            Message msgDialog = new MessageDialog(winner + " wins!!\n" + loser + "lost", "Congrats!");
+            msgDialog.ShowAsync();
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_Told_When_A_Draw_Happens()
+        {
+            there_is_winner = false;
+
+            Message msgDialog = new MessageDialog("It's a draw", "Sad!");
+            msgDialog.ShowAsync();
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_Queried_If_They_Wish_To_Pay_Again()
+        {
+            MessageDialog msgDialog = new MessageDialog(winner + " wins!!", "Congrats!");
+            msgDialog.ShowAsync();
+            msgDialog.Commands.Add(new UICommand("Play again", new UICommandInvokedHandler(this.CommandInvokedHandler1)));
+            msgDialog.Commands.Add(new UICommand("Close", new UICommandInvokedHandler(this.CommandInvokedHandler2)));
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_Enter_Their_Name_Before_Game_Starts()
+        {
+        }
+
+        [TestMethod]
+        public void A_User_Needs_To_See_A_Score_Chart()
+        {
+        }
+
+       
+
+    }
+}
